9d9c444d2c922e5fb48d5e0e40d155b8f09c37f6
niharika4092 <68004005+niharika4092@users.noreply.github.com>
2020-07-08 14:52:44 +0530

    Create swap.c
    this file swaps 2 numbers entered by user

---
+++ swap.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z;
+printf("enter two integers\n");
+scanf("%d%d", &x, &y);
+printf("before swapping\nfirst integer = %d\nsecond integer = %d\n", x, y);
+return 0;
+}
